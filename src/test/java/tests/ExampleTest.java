881f8e99c03b8e0f7972b75190ddb5344675a43f
v.elara <viktor.elara@madnesspartners.com>
2025-06-04 16:30:00 +0400

    commit 2

--- src/test/java/tests/ExampleTest.java
+++ src/test/java/tests/ExampleTest.java
@@ -1,15 +1,15 @@
 package tests;
 
+import aquality.tracking.integrations.core.AqualityTrackingLifecycle;
 import io.qameta.allure.Allure;
 import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
 import io.restassured.response.Response;
 import logger.LogConfigExtractor;
 import org.apache.http.HttpStatus;
 import org.testng.ITestContext;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Test;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 import org.testng.reporters.JUnitXMLReporter;
 import steps.api.ExampleSteps;
 import utils.allure.AllureEnvironment;
@@ -19,23 +19,34 @@ import java.io.FileNotFoundException;
 
 public class ExampleTest {
 
-    @BeforeClass
+    private final AqualityTrackingLifecycle tracking = new AqualityTrackingLifecycle();
+
+    @BeforeTest
     public void setupListeners(ITestContext context) {
+        tracking.startTestRun();
         context.getSuite().addListener(new JUnitXMLReporter());
     }
 
+    @AfterTest
+    public void testTearDown(ITestContext context) {
+        tracking.finishTestRun();
+    }
+
     @BeforeMethod
-    public void setUp() {
+    public void setUp(ITestContext context) {
+        tracking.startTestExecution(context.getName());
         AllureEnvironment.clearFile();
         AllureEnvironment.setValue("BASE_URL", ExampleSteps.BASE_URL);
     }
 
     @AfterMethod
-    public void addAttachment() throws FileNotFoundException {
+    public void addAttachment(ITestResult testResult) throws FileNotFoundException {
+        tracking.finishTestExecution(testResult.getStatus(), "Fail Reason Message");
         Allure.addAttachment("Log", new FileInputStream(LogConfigExtractor.LOG_FILE_PATH));
     }
 
     @Test(description = "JsonPlaceholder API Test")
+    @Issue("DEFECT-1")
     @Description("JsonPlaceholder API testing")
     public void exampleTest() {
         Response response = ExampleSteps.request(1);
